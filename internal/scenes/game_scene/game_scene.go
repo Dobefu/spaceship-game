4aed53220c276ed7ca13ff0642c749e8422e1aae
Connor van Spronssen <info@dobefu.com>
2025-06-01 16:10:07 +0200

    feat: Add a game scene

---
+++ internal/scenes/game_scene/game_scene.go
@@ -0,0 +1,14 @@
+package game_scene
+
+import (
+	"github.com/Dobefu/spaceship-game/internal/player"
+	"github.com/Dobefu/spaceship-game/internal/scene"
+)
+
+type GameScene struct {
+	scene.Scene
+}
+
+func (s *GameScene) Init() {
+	s.AddGameObject(player.Player{})
+}
