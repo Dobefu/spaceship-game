5c24233f2160ec939d5142b4a1739599cc38f729
Connor van Spronssen <info@dobefu.com>
2025-06-01 16:42:07 +0200

    feat: Add a model for the player

--- internal/player/player.go
+++ internal/player/player.go
@@ -1,15 +1,51 @@
 package player
 
 import (
+	"image"
+	"image/color"
+
 	"github.com/Dobefu/spaceship-game/internal/game_object"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
+
+var (
+	modelPath       vector.Path
+	modelPathPoints [][2]float32
+
+	whiteImage    = ebiten.NewImage(3, 3)
+	whiteSubImage = whiteImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
+
+	strokeOptions        *vector.StrokeOptions
+	drawTrianglesOptions *ebiten.DrawTrianglesOptions
+)
+
+func init() {
+	modelPathPoints = [][2]float32{
+		{50, 0},
+		{100, 100},
+		{50, 75},
+		{0, 100},
+	}
+
+	whiteImage.Fill(color.White)
+	strokeOptions = &vector.StrokeOptions{
+		Width: 2,
+	}
+
+	drawTrianglesOptions = &ebiten.DrawTrianglesOptions{
+		AntiAlias: true,
+	}
+}
 
 type Player struct {
 	game_object.IGameObject
 
 	X float32
 	Y float32
+
+	vertices []ebiten.Vertex
+	indices  []uint16
 }
 
 func (p Player) Update() (err error) {
@@ -17,4 +53,14 @@ func (p Player) Update() (err error) {
 }
 
 func (p Player) Draw(screen *ebiten.Image) {
+	modelPath = vector.Path{}
+
+	for _, points := range modelPathPoints {
+		modelPath.LineTo(points[0]+p.X, points[1]+p.Y)
+	}
+
+	modelPath.Close()
+
+	p.vertices, p.indices = modelPath.AppendVerticesAndIndicesForStroke(p.vertices[:0], p.indices[:0], strokeOptions)
+	screen.DrawTriangles(p.vertices, p.indices, whiteSubImage, drawTrianglesOptions)
 }
