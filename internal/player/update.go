d56c014eb500c06b71c9dcb5c3a655b77416ff0f
Connor van Spronssen <info@dobefu.com>
2025-06-01 18:10:56 +0200

    feat: Add forward and backward movement

--- internal/player/update.go
+++ internal/player/update.go
@@ -1,6 +1,11 @@
 package player
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"math"
+
+	"github.com/Dobefu/spaceship-game/internal/vectors"
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 func (p *Player) Update() (err error) {
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
@@ -9,6 +14,25 @@ func (p *Player) Update() (err error) {
 
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		p.rotation += .1
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyDown) {
+		sin := float32(math.Sin(p.rotation - math.Pi/2))
+		cos := float32(math.Cos(p.rotation - math.Pi/2))
+
+		if ebiten.IsKeyPressed(ebiten.KeyUp) {
+			p.velocity.Add(vectors.Vector2{
+				X: cos * .1,
+				Y: sin * .1,
+			})
+		}
+
+		if ebiten.IsKeyPressed(ebiten.KeyDown) {
+			p.velocity.Sub(vectors.Vector2{
+				X: cos * .1,
+				Y: sin * .1,
+			})
+		}
 	}
 
 	p.position.Add(p.velocity)
