f3ac8a43c2732b28b3d1e460f390b5aa5c7e9977
Connor van Spronssen <info@dobefu.com>
2025-06-17 08:11:50 +0200

    refactor: Move the fire model offsets to the update function, so the animation pauses along with the game

--- internal/player/draw_fire_model.go
+++ internal/player/draw_fire_model.go
@@ -3,7 +3,6 @@ package player
 import (
 	"math"
 
-	"github.com/Dobefu/spaceship-game/internal/fastrand"
 	"github.com/Dobefu/spaceship-game/internal/globals"
 	"github.com/Dobefu/spaceship-game/internal/vectors"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -11,8 +10,9 @@ import (
 )
 
 var (
-	fireModelPath       vector.Path
-	fireModelPathPoints []vectors.Vector2
+	fireModelPath        vector.Path
+	fireModelPathPoints  []vectors.Vector2
+	fireModelPathOffsets []vectors.Vector2
 )
 
 func init() {
@@ -28,6 +28,14 @@ func init() {
 		{X: 14, Y: 16},
 		{X: 17, Y: 10},
 	}
+
+	// Pre-fill the offsets slice, to get allocations out of the way.
+	for range fireModelPathPoints {
+		fireModelPathOffsets = append(
+			fireModelPathOffsets,
+			vectors.Vector2{X: 0, Y: 0},
+		)
+	}
 }
 
 func (p *Player) drawFireModel(screen *ebiten.Image) {
@@ -40,15 +48,12 @@ func (p *Player) drawFireModel(screen *ebiten.Image) {
 	camera := globals.GlobalValues.Game.GetScene().GetCamera()
 	position := p.GetPosition()
 
-	for _, points := range fireModelPathPoints {
+	for i, points := range fireModelPathPoints {
 		sin := math.Sin(p.rotation)
 		cos := math.Cos(p.rotation)
 
-		randX := float64(fastrand.Rand.Next()>>24)/1270 + 1
-		randY := float64(fastrand.Rand.Next()>>24)/1000 + 1
-
-		x := ((points.X - playerModelCenter.X) * p.fireScale) * randX
-		y := ((points.Y-8)*p.fireScale + 8) * randY
+		x := ((points.X - playerModelCenter.X) * p.fireScale) * fireModelPathOffsets[i].X
+		y := ((points.Y-8)*p.fireScale + 8) * fireModelPathOffsets[i].Y
 
 		screenPos := camera.WorldToScreenPosition2D(vectors.Vector3{
 			X: ((x*cos - y*sin) + position.X) * p.scale,
--- internal/player/update.go
+++ internal/player/update.go
@@ -3,11 +3,20 @@ package player
 import (
 	"math"
 
+	"github.com/Dobefu/spaceship-game/internal/fastrand"
 	"github.com/Dobefu/spaceship-game/internal/input"
 )
 
 func (p *Player) Update() (err error) {
 	p.HandleMovement()
+
+	for i := range fireModelPathPoints {
+		randX := float64(fastrand.Rand.Next()>>24)/1270 + 1
+		randY := float64(fastrand.Rand.Next()>>24)/1000 + 1
+
+		fireModelPathOffsets[i].X = randX
+		fireModelPathOffsets[i].Y = randY
+	}
 
 	p.shootCooldown = math.Max(p.shootCooldown-1, 0)
 
