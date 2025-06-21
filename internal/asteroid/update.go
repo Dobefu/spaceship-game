6d0d459eea4e5bf297b9b1e101f45ce8a3bf0e07
Connor van Spronssen <info@dobefu.com>
2025-06-21 12:57:59 +0200

    feat: Remove asteroids that are off-screen

--- internal/asteroid/update.go
+++ internal/asteroid/update.go
@@ -1,5 +1,16 @@
 package asteroid
 
+import "github.com/Dobefu/spaceship-game/internal/globals"
+
 func (a *Asteroid) Update() (err error) {
+	camera := globals.GlobalValues.Game.GetScene().GetCamera()
+	screenPos := camera.WorldToScreenPosition2D(a.GetPosition())
+
+	// If the asteroid is off-screen, deactivate it.
+	if camera.IsPositionWithinBounds(screenPos, 10*a.scale) {
+		a.SetIsActive(false)
+		return nil
+	}
+
 	return nil
 }
