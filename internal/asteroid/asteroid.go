ab4f4f87e69f8d9856525e16bf7d9340579f4902
Connor van Spronssen <info@dobefu.com>
2025-06-21 12:55:41 +0200

    refactor: Move the asteroid vertex randomization to individual asteroid instances

--- internal/asteroid/asteroid.go
+++ internal/asteroid/asteroid.go
@@ -1,8 +1,13 @@
 package asteroid
 
 import (
+	"math"
+
+	"github.com/Dobefu/spaceship-game/internal/fastrand"
 	"github.com/Dobefu/spaceship-game/internal/game_object"
+	"github.com/Dobefu/spaceship-game/internal/vectors"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Asteroid struct {
@@ -10,6 +15,10 @@ type Asteroid struct {
 
 	rotation float64
 	scale    float64
+
+	asteroidModelPath       vector.Path
+	asteroidModelPathPoints []vectors.Vector2
+	asteroidModelCenter     vectors.Vector2
 
 	vertices []ebiten.Vertex
 	indices  []uint16
@@ -19,6 +28,27 @@ func NewAsteroid() (asteroid *Asteroid) {
 	asteroid = &Asteroid{
 		scale: 1,
 	}
+
+	asteroid.asteroidModelPathPoints = []vectors.Vector2{}
+
+	const pointCount = 24
+	for i := range pointCount {
+		randX := float64(fastrand.Rand.Next()>>28) / 4
+		randY := float64(fastrand.Rand.Next()>>28) / 4
+
+		asteroid.asteroidModelPathPoints = append(asteroid.asteroidModelPathPoints, vectors.Vector2{
+			X: math.Cos((float64(i)/pointCount)*(math.Pi*2)) * (randX + 20),
+			Y: math.Sin((float64(i)/pointCount)*(math.Pi*2)) * (randY + 20),
+		})
+	}
+
+	for _, points := range asteroid.asteroidModelPathPoints {
+		asteroid.asteroidModelCenter.X += points.X
+		asteroid.asteroidModelCenter.Y += points.Y
+	}
+
+	asteroid.asteroidModelCenter.X /= float64(len(asteroid.asteroidModelPathPoints))
+	asteroid.asteroidModelCenter.Y /= float64(len(asteroid.asteroidModelPathPoints))
 
 	return asteroid
 }
--- internal/asteroid/draw.go
+++ internal/asteroid/draw.go
@@ -4,7 +4,6 @@ import (
 	"image/color"
 	"math"
 
-	"github.com/Dobefu/spaceship-game/internal/fastrand"
 	"github.com/Dobefu/spaceship-game/internal/globals"
 	"github.com/Dobefu/spaceship-game/internal/vectors"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,38 +11,11 @@ import (
 )
 
 var (
-	asteroidModelPath       vector.Path
-	asteroidModelPathPoints []vectors.Vector2
-	asteroidModelCenter     vectors.Vector2
-
 	whiteImage = ebiten.NewImage(1, 1)
 
 	strokeOptions        *vector.StrokeOptions
 	drawTrianglesOptions *ebiten.DrawTrianglesOptions
 )
-
-func init() {
-	asteroidModelPathPoints = []vectors.Vector2{}
-
-	const pointCount = 24
-	for i := range pointCount {
-		randX := float64(fastrand.Rand.Next()>>28) / 4
-		randY := float64(fastrand.Rand.Next()>>28) / 4
-
-		asteroidModelPathPoints = append(asteroidModelPathPoints, vectors.Vector2{
-			X: math.Cos((float64(i)/pointCount)*(math.Pi*2)) * (randX + 20),
-			Y: math.Sin((float64(i)/pointCount)*(math.Pi*2)) * (randY + 20),
-		})
-	}
-
-	for _, points := range asteroidModelPathPoints {
-		asteroidModelCenter.X += points.X
-		asteroidModelCenter.Y += points.Y
-	}
-
-	asteroidModelCenter.X /= float64(len(asteroidModelPathPoints))
-	asteroidModelCenter.Y /= float64(len(asteroidModelPathPoints))
-}
 
 func init() {
 	whiteImage.Set(0, 0, color.White)
@@ -58,16 +30,16 @@ func init() {
 }
 
 func (a *Asteroid) Draw(screen *ebiten.Image) {
-	asteroidModelPath = vector.Path{}
+	a.asteroidModelPath = vector.Path{}
 	position := a.GetPosition()
 	camera := globals.GlobalValues.Game.GetScene().GetCamera()
 
-	for _, points := range asteroidModelPathPoints {
+	for _, points := range a.asteroidModelPathPoints {
 		sin := math.Sin(a.rotation)
 		cos := math.Cos(a.rotation)
 
-		x := (points.X - asteroidModelCenter.X)
-		y := (points.Y - asteroidModelCenter.Y)
+		x := (points.X - a.asteroidModelCenter.X)
+		y := (points.Y - a.asteroidModelCenter.Y)
 
 		screenPos := camera.WorldToScreenPosition2D(vectors.Vector3{
 			X: ((x*cos - y*sin) + position.X) * a.scale,
@@ -75,14 +47,14 @@ func (a *Asteroid) Draw(screen *ebiten.Image) {
 			Z: position.Z,
 		})
 
-		asteroidModelPath.LineTo(
+		a.asteroidModelPath.LineTo(
 			float32(screenPos.X),
 			float32(screenPos.Y),
 		)
 	}
 
-	asteroidModelPath.Close()
+	a.asteroidModelPath.Close()
 
-	a.vertices, a.indices = asteroidModelPath.AppendVerticesAndIndicesForStroke(a.vertices[:0], a.indices[:0], strokeOptions)
+	a.vertices, a.indices = a.asteroidModelPath.AppendVerticesAndIndicesForStroke(a.vertices[:0], a.indices[:0], strokeOptions)
 	screen.DrawTriangles(a.vertices, a.indices, whiteImage, drawTrianglesOptions)
 }
